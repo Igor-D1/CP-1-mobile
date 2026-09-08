5706bac79b60e41842d25c8e144d1aa96f808ab6
Igor-D1 <RM568337@fiap.com.br>
2026-09-08 18:31:24 -0300

    Finalizando cp-1 de mobile

--- mobile_cp/lib/main.dart
+++ mobile_cp/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: "Restaurante Mesa Cheia",
       debugShowCheckedModeBanner: false,
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget{
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -23,15 +25,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int contador = 0;
+  bool image = false; // Controle de visibilidade da imagem do botão
 
-  void decrementador(){
-    setState(()=> contador--);
+  void decrementador() {
+    setState(() => contador--);
   }
-  void incrementador(){
-    setState(()=> contador++);
+
+  void incrementador() {
+    setState(() => contador++);
   }
-  void limpador(){
-    setState(()=> contador = 0);
+
+  void limpador() {
+    setState(() => contador = 0);
   }
 
   bool get isFull => contador == 50;
@@ -39,22 +44,31 @@ class _HomePageState extends State<HomePage> {
   bool get isEmpty => contador == 0;
   bool get minimo => contador >= 10;
 
-  String getBackgroundImage(bool isWideScreen, bool isFull){
-    if(isWideScreen){
-      return isFull ? "assets/images/limite_desktop_bg.png" : "assets/images/restaurante_desktop_bg.png";
+  String getBackgroundImage(bool isWideScreen, bool isFull) {
+    if (isWideScreen) {
+      return isFull
+          ? "assets/images/limite_desktop_bg.png"
+          : "assets/images/restaurante_desktop_bg.png";
     } else {
-      return isFull ? "assets/images/limite_mobile_bg.png" : "assets/images/restaurante_mobile_bg.png";
+      return isFull
+          ? "assets/images/limite_mobile_bg.png"
+          : "assets/images/restaurante_mobile_bg.png";
     }
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final bool isWideScreen = MediaQuery.of(context).size.width > 600;
-
     final String imagePath = getBackgroundImage(isWideScreen, isFull);
 
-    return Scaffold(appBar: AppBar(
-      title: Text("Restaurante Mesa Cheia ", style: TextStyle(fontSize:30, color: Colors.white)
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Restaurante Mesa Cheia",
+          style: GoogleFonts.eater(
+            fontSize: isWideScreen ? 35 : 20,
+            color: Colors.white,
+          ),
         ),
         backgroundColor: Colors.black,
         centerTitle: true,
@@ -65,75 +79,137 @@ class _HomePageState extends State<HomePage> {
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage(imagePath),
-            fit:BoxFit.cover,
+            fit: BoxFit.cover,
             alignment: Alignment.bottomCenter,
             colorFilter: ColorFilter.mode(
               Colors.black.withAlpha(100),
               BlendMode.darken,
-            )
-            )
+            ),
+          ),
         ),
-        child: Column(
-          mainAxisAlignment:
-            MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Text(
-              isFull ? "" : isEmpty ? "Restaurante está vazio" : quaseCheio ? "Restaurante quase cheio" : "Ainda há vagas",
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.white,
-                fontWeight: FontWeight.w700,
+            Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      isFull ? "" : isEmpty ? "Restaurante está vazio" : quaseCheio ? "Restaurante quase cheio" : "Ainda há vagas",
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      contador.toString(),
+                      style: TextStyle(fontSize: 100, color: Colors.white),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: isEmpty ? null : decrementador,
+                          style: TextButton.styleFrom(
+                            backgroundColor: isEmpty
+                                ? Colors.white.withAlpha(70)
+                                : Colors.white,
+                            fixedSize: Size(141, 55),
+                          ),
+                          child: Text(
+                            isEmpty ? "Vazio" : "Saiu",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: isEmpty ? Colors.red : Colors.black,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        TextButton(
+                          onPressed: isFull ? null : incrementador,
+                          style: TextButton.styleFrom(
+                            backgroundColor: isFull
+                                ? Colors.white.withAlpha(70)
+                                : Colors.white,
+                            fixedSize: Size(141, 55),
+                          ),
+                          child: Text(
+                            isFull ? "Lotado" : "Entrou",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: isFull ? Colors.red : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    TextButton(
+                      onPressed: minimo ? limpador : null,
+                      style: TextButton.styleFrom(
+                        backgroundColor: minimo
+                            ? Colors.white
+                            : Colors.white.withAlpha(70),
+                        fixedSize: Size(141, 55),
+                      ),
+                      child: Text(
+                        "Limpar contador",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: minimo ? Colors.black : Colors.red,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            Text(contador.toString(),
-            style:
-            TextStyle(fontSize:100, color: Colors.white)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: isEmpty ? null : decrementador,
-                  style: TextButton.styleFrom(
-                    backgroundColor: isEmpty ?Colors.white.withAlpha(70) : Colors.white,
-                    fixedSize: Size(141, 55),
-                  ),
-                  child: Text(
-                    isEmpty? "Vazio" : "Saiu",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16, color: isEmpty? Colors.red : Colors.black),
-                  ),
-                  ),
-                  const SizedBox(width: 20),
-                  TextButton(
-                    onPressed: isFull ? null : incrementador,
-                    style: TextButton.styleFrom(
-                      backgroundColor: isFull? Colors.white.withAlpha(70) : Colors.white,
-                      fixedSize: Size(141, 55),
-                    ),
-                    child: Text(
-                      isFull ? "Lotado" : "Entrou",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16, color: isFull? Colors.red : Colors.black)
+
+            Positioned(
+              left: 10,
+              bottom: 20,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Botão circular pequeno
+                  FloatingActionButton.small(
+                    onPressed: () {
+                      setState(() {
+                        image = !image;
+                      });
+                    },
+                    child: Icon(
+                      image ? Icons.close : Icons.help_outline,
+                      color: Colors.black,
                     ),
                   ),
-              ],
+
+                  const SizedBox(width: 12),
+
+                  // Imagem exibida ao lado do botão quando pressionado
+                  if (image)
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 2),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Image.asset(
+                          'assets/icon/Dodo_prova.png', // Ajuste para a sua imagem
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
             ),
-            const SizedBox(width: 20,height: 24),
-                  TextButton(
-                    onPressed: minimo ? limpador : null,
-                    style: TextButton.styleFrom(
-                      backgroundColor: minimo
-                          ? Colors.white
-                          : Colors.white.withAlpha(70),
-                      fixedSize: Size(141, 55),
-                      
-                    ),
-                    child: Text(
-                      "Limpar contador",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16, color: minimo ? Colors.black : Colors.red),
-                    ),
-                  ),
           ],
         ),
       ),
